bc24af2b044b70ed3f90a238ba639750f3668e5e
j0rsche <78011741+tehjosher@users.noreply.github.com>
2024-10-11 14:58:05 -0400

    leftboxinfo
    Information for content of left box in super macro

---
+++ leftboxcode-info.lua
@@ -0,0 +1,10 @@
+/script if false then CastSpellByName("Stormstrike") end
+/script WeaponSwap("*YOUR 2HAND WEAPON HERE*"); --#1
+/script WeaponSwap("*YOUR 1HAND WEAPON HERE*", "YOUR SHIELD/OFFHAND HERE*"); --#2
+/script shaman_1button()
+/attack
+
+
+
+-- This is a SAMPLE of what text would go in the left box. You would replace the *YOUR WEAPON HERE* with whatever weapon you want to equip if you so desire.
+-- You would use EITHER #1 or #2 for whichever weapon setup you want. You can also change the "Stormstrike" to whatever is in your spell book you would like to use as the icon for this macro.
