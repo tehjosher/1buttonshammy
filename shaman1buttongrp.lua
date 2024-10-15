c6f2b44b1ea3103229f1a51b3e373c8b9760b9c7
j0rsche <78011741+tehjosher@users.noreply.github.com>
2024-10-15 18:36:46 -0400

    Create shaman1buttongrp.lua
    added group option with flame shock and no weapon buff

---
+++ shaman1buttongrp.lua
@@ -0,0 +1,69 @@
+function shaman_1button()
+    -- spell names
+    local water_shield = "Water Shield"
+    local stormstrike = "Stormstrike"
+    local flame_shock = "Flame Shock"
+ --   local windfury_weapon = "Windfury Weapon"
+
+    -- Stormstrike on cooldown.
+    if cooldown_for(stormstrike) == 0 then
+        cast(stormstrike)
+    end
+
+    -- Keep up Water Shield.
+    if not buffed(water_shield) then
+        cast(water_shield)
+        return
+    end
+
+    -- Keep up Windfury Weapon.
+--    if not buffed("Windfury", 'player') then
+--        cast(windfury_weapon)
+--        return
+--    end
+
+    -- Cast and apply Flame Shock debuff.
+    if not buffed("Flame Shock", 'target') then
+        cast_and_downrank(flame_shock)
+        return
+    end
+end
+
+function cooldown_for(spell_name)
+        local index = spell_index(spell_name)
+        local _, cd, _ = GetSpellCooldown(index, BOOKTYPE_SPELL)
+        return cd
+end
+
+    
+function spell_index(spell_name, spell_rank)
+    local i = 1
+    while true do
+        local name, rank = GetSpellName(i, BOOKTYPE_SPELL)
+        if not name then
+            error(string.format("spell not found: %s, scanned: %d", spell_name, i))
+            return
+        end
+    
+        if (not spell_rank and name == spell_name) or (name == spell_name and rank == spell_rank) then
+            return i
+        end
+    
+        i = i + 1
+    end
+end
+
+-- If under 60% mana, cast the Rank 4 version of a spell. Under 20%, cast the Rank 1 Version.
+function cast_and_downrank(spell_name)
+    local mana = UnitMana("player")
+    local max_mana = UnitManaMax("player")
+    if mana/max_mana < 0.2 then
+        spell_name = string.format("%s(Rank 1)", spell_name)
+
+    elseif mana/max_mana < 0.6 then
+     
+        spell_name = string.format("%s(Rank 4)", spell_name)
+    end
+
+    cast(spell_name)
+end
